f7d8ab0f151a29ebf1d7ad7ab502a9a1ac4633d4
Gaurav Singh <112081659+Gauravkazu@users.noreply.github.com>
2022-12-03 18:11:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function computerPlay() 
+{
+    const pick = ['rock', 'paper', 'scissors'];
+    return pick[Math.floor(Math.random() * pick.length)];
+ }
+ 
+ function playRound(playerSelection, computerSelection) 
+ {
+if (playerSelection === computerSelection) {
+return `It's a tie! you both picked {playerSelection}`;
+ } 
+ else if (playerSelection === "rock" && computerSelection === "scissors") 
+ {
+ return "You win! Rock beats Scissors";
+}
+ else if (playerSelection === "paper" && computerSelection === "rock") 
+ {
+ return "You win! Paper beats Rock";
+} 
+else if (playerSelection === "scissors" && computerSelection === "paper") 
+{
+return "You win! Scissors beats Paper";
+} 
+else 
+{
+return `You lose! {computerSelection} beats {playerSelection}`;
+}
+ }
+const playerSelection = prompt("Pick Rock, Paper, Scissors").toLowerCase();
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection))
+function game() {
+for (let i = 0; i < 5; i++) {
+console.log(playerSelection, computerSelection);
+}
+}
+game();
